025e812cee433d19b5e84ec119c2c9c7ce3b1e8d
ADoyle <adoyle.h@gmail.com>
2024-09-12 19:37:05 +0800

    fix: change folders for one.bash v1

--- one.repo.bash
+++ one.repo.bash
@@ -1,16 +1,16 @@
-name=one-bash-it
+ABOUT='one.bash repo for bash-it. https://github.com/Bash-it/bash-it'
 
 repo_add_post() {
-  print_verb "[Todo] git clone bash-it"
-  git clone https://github.com/Bash-it/bash-it.git bash-it
+	print_verb "[Todo] git clone bash-it"
+	git clone https://github.com/Bash-it/bash-it.git bash-it
 }
 
-repo_update() {
-  print_verb "[Todo] git -C bash-it pull"
-  git -C bash-it pull
+repo_update_post() {
+	print_verb "[Todo] git -C bash-it pull"
+	git -C bash-it pull
 }
 
 repo_onload() {
-  # shellcheck source=./bash-it/bash_it.sh
-  _one_load "$CUR_REPO_DIR"/bash-it/bash_it.sh
+	# shellcheck source=./bash-it/bash_it.sh
+	_one_load "$CUR_REPO_DIR"/bash-it/bash_it.sh
 }
